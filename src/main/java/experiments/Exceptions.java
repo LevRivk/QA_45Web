32d22a389704608e7beda5bfded2ba19d93e5765
leva <shendonlevka@gmail.com>
2024-11-17 19:23:53 +0200

    Created Exeptions 1

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -2,5 +2,35 @@ package experiments;
 
 public class Exceptions {
 
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedException(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void uncheckedException(String[] strArray) {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(strArray[i]);
+            }
+            System.out.println("programm is working");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("created exeption");
+        }
+    }
+    private static void pause() throws InterruptedException {
+        checkedExeption();
+        System.out.println("pause 3 sec");
+    }
+
+    private static void checkedExeption() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
 
 }
